fbd46126b2925b3e79fd6d26169f593f0821c3ab
4802852 <4802852@gmail.com>
2021-08-04 02:11:23 +0900

    kiwoomAPI AutoTrader Version1.0 complete

--- KiwoomAPI/kiwoom-1.py
+++ KiwoomAPI/kiwoom-1.py
@@ -4,6 +4,7 @@ from PyQt5.QAxContainer import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import datetime
+import time
 from config.errCode import *
 
 
@@ -24,6 +25,11 @@ class MyWindow(QMainWindow):
         self.amount = None
         self.account = None
         self.hold = None
+        self.on_market = None
+
+        t_now = datetime.datetime.now()
+        self.t_start = t_now.replace(hour=9, minute=5, second=0, microsecond=0)
+        self.t_sell = t_now.replace(hour=15, minute=15, second=0, microsecond=0)
 
         self.plain_text_edit = QPlainTextEdit(self)
         self.plain_text_edit.setReadOnly(True)
@@ -49,14 +55,31 @@ class MyWindow(QMainWindow):
         self.account = accounts.split(";")[0]
         self.plain_text_edit.appendPlainText(f"현재 계좌번호: {self.account}")
 
-        # TR 요청
+        self.subscribe_market_time("1")
+
         for symbol in symbol_list:
-            self.request_opt10081(symbol)
-        self.request_opw00001()
-        self.request_opw00004()
+            self.request_opt10081(symbol)  # 종목 별 전일 정보 조회
+        self.request_opw00004()  # 계좌 평가 현황 조회
+
+        start = 1
+        while start:
+            t_now = datetime.datetime.now()
+            if self.on_market == "0":
+                self.plain_text_edit.appendPlainText("장 시작 전입니다.")
+            elif self.on_market == ("2" or "4"):
+                QCoreApplication.instance().quit()
+                print("장 종료되었습니다.")
+            elif self.on_market == "3":
+                if t_now < self.t_start:
+                    if self.bought_list:
+                        self.sell_all(self.bought_list)
+                elif self.t_start < t_now < self.t_sell:
+                    start = 0
+            time.sleep(10)
+
+        self.request_opw00001()  # 예수금 조회
 
         # 주식체결 (실시간)
-        self.subscribe_market_time("1")
         for i, symbol in enumerate(self.symbol_list):
             self.subscribe_stock_conclusion(str(i + 1), symbol)
 
@@ -88,12 +111,12 @@ class MyWindow(QMainWindow):
             for i in range(rows):
                 종목코드 = self.GetCommData(trcode, rqname, i, "종목코드")
                 보유수량 = self.GetCommData(trcode, rqname, i, "보유수량")
-
+                self.bought_list.append(종목코드)
                 self.symbol_dict[종목코드[1:]][4] = int(보유수량)
 
         else:
-            now = datetime.datetime.now()
-            today = now.strftime("%Y%m%d")
+            t_now = datetime.datetime.now()
+            today = t_now.strftime("%Y%m%d")
             일자 = self.GetCommData(trcode, rqname, 0, "일자")
 
             # 장시작 후 TR 요청하는 경우 0번째 row에 당일 일봉 데이터가 존재함
@@ -158,66 +181,80 @@ class MyWindow(QMainWindow):
         if real_type == "장시작시간":
             장운영구분 = self.GetCommRealData(code, 215)
             if 장운영구분 == "3":
-                if self.previous_day_hold:
-                    self.previous_day_hold = False
-                    # 매도 (시장가)
-                    self.SendOrder(
-                        "매도",
-                        "8001",
-                        self.account,
-                        2,
-                        "229200",
-                        self.previous_day_quantity,
-                        0,
-                        "03",
-                        "",
-                    )
-            elif 장운영구분 == "4":
+                self.on_market = 장운영구분
+            elif 장운영구분 == ("2" or "4"):
+                self.on_market = 장운영구분
                 QCoreApplication.instance().quit()
                 print("메인 윈도우 종료")
 
         elif real_type == "주식체결":
-            # 현재가
-            현재가 = self.GetCommRealData(code, 10)
-            현재가 = abs(int(현재가))  # +100, -100
-            체결시간 = self.GetCommRealData(code, 20)
+            t_now = datetime.datetime.now()
 
-            # 목표가 계산
-            # TR 요청을 통한 전일 range가 계산되었고 아직 당일 목표가가 계산되지 않았다면
-            if self.symbol_dict[code][1] and self.symbol_dict[code][2] == 0:
-                시가 = self.GetCommRealData(code, 16)
-                시가 = abs(int(시가))  # +100, -100
-                code_target = int(시가 + (self.range * 0.4))
-                self.symbol_dict[code][2] = code_target
-                self.plain_text_edit.appendPlainText(f"{code} 목표가 계산됨: {code_target}")
+            if self.t_sell < t_now:
+                if self.bought_list:
+                    self.sell_all(self.bought_list)
+                time.sleep(10)
+                QCoreApplication.instance().quit()
+                print("메인 윈도우 종료")
+            else:
+                # 현재가
+                현재가 = self.GetCommRealData(code, 10)
+                현재가 = abs(int(현재가))  # +100, -100
+                체결시간 = self.GetCommRealData(code, 20)
 
-            # 매수시도
-            # 당일 매수하지 않았고
-            # TR 요청과 Real을 통한 목표가가 설정되었고
-            # TR 요청을 통해 잔고조회가 되었고
-            # 현재가가 목표가가 이상이면
-            if (
-                self.symbol_dict[code][3] is False
-                and self.symbol_dict[code][2]
-                and self.amount is not None
-                and 현재가 >= self.symbol_dict[code][2]
-            ):
-                self.symbol_dict[code][3] = True
-                quantity = int(self.amount / 현재가)
-                self.SendOrder("매수", "8000", self.account, 1, code, quantity, 0, "03", "")
-                self.plain_text_edit.appendPlainText(f"{self.symbol_dict[code][0]} 시장가 매수 진행 수량: {quantity}")
-                self.bought_list.append(code)
+                # 목표가 계산
+                # TR 요청을 통한 전일 range가 계산되었고 아직 당일 목표가가 계산되지 않았다면
+                if self.symbol_dict[code][1] and self.symbol_dict[code][2] == 0:
+                    시가 = self.GetCommRealData(code, 16)
+                    시가 = abs(int(시가))  # +100, -100
+                    code_target = int(시가 + (self.range * 0.4))
+                    self.symbol_dict[code][2] = code_target
+                    self.plain_text_edit.appendPlainText(f"{code} 목표가 계산됨: {code_target}")
 
-            # 로깅
-            self.plain_text_edit.appendPlainText(
-                f"{self.symbol_dict[code][0]} 시간: {체결시간} 목표가: {self.symbol_dict[code][2]} 현재가: {현재가} 보유여부: {self.symbol_dict[code][3]}"
-            )
+                # 매수시도
+                # 당일 매수하지 않았고
+                # TR 요청과 Real을 통한 목표가가 설정되었고
+                # TR 요청을 통해 잔고조회가 되었고
+                # 현재가가 목표가가 이상이면
+                if (
+                    self.symbol_dict[code][3] is False
+                    and self.symbol_dict[code][2]
+                    and self.amount is not None
+                    and 현재가 >= self.symbol_dict[code][2]
+                ):
+                    self.symbol_dict[code][3] = True
+                    quantity = int(self.amount / 현재가)
+                    self.SendOrder("매수", "8000", self.account, 1, code, quantity, 0, "03", "")
+                    self.plain_text_edit.appendPlainText(
+                        f"{self.symbol_dict[code][0]} 시장가 매수 진행 수량: {quantity}"
+                    )
+                    self.bought_list.append(code)
+
+                # 로깅
+                self.plain_text_edit.appendPlainText(
+                    f"{self.symbol_dict[code][0]} 시간: {체결시간} 목표가: {self.symbol_dict[code][2]} 현재가: {현재가} 보유여부: {self.symbol_dict[code][3]}"
+                )
 
     def _handler_chejan_data(self, gubun, item_cnt, fid_list):
         if "gubun" == "1":  # 잔고통보
             예수금 = self.GetChejanData("951")
             self.amount = int(예수금)
             self.plain_text_edit.appendPlainText(f"투자금액 업데이트 됨: {self.amount}")
+
+    def sell_all(self, bought_list):
+        for symbol in bought_list:
+            self.SendOrder(
+                "매도",
+                "8001",
+                self.account,
+                2,
+                symbol,
+                self.symbol_list[symbol][4],
+                0,
+                "03",
+                "",
+            )
+        self.bought_list = []
 
     def subscribe_stock_conclusion(self, screen_no, symbol):
         self.SetRealReg(screen_no, symbol, "20", 1)
@@ -235,7 +272,11 @@ class MyWindow(QMainWindow):
 
     def CommRqData(self, rqname, trcode, next, screen_no):
         self.ocx.dynamicCall(
-            "CommRqData(QString, QString, int, QString)", rqname, trcode, next, screen_no
+            "CommRqData(QString, QString, int, QString)",
+            rqname,
+            trcode,
+            next,
+            screen_no,
         )
 
     def GetCommData(self, trcode, rqname, index, item):
--- KiwoomAPI/test.py
+++ KiwoomAPI/test.py
@@ -1,10 +1,14 @@
-symbol = ["123", "234", "345", "456"]
-dict = {}
-for i in range(len(symbol)):
-    dict[symbol[i]] = [0, 0, 0]
-print(dict)
-dict["234"][2] = 1
-print(dict)
-accum = ";".join(symbol) + ";"
-print(accum)
-print(type(accum))
+# symbol = ["123", "234", "345", "456"]
+# dict = {}
+# for i in range(len(symbol)):
+#     dict[symbol[i]] = [0, 0, 0]
+# print(dict)
+# dict["234"][2] = 1
+# print(dict)
+# accum = ";".join(symbol) + ";"
+# print(accum)
+# print(type(accum))
+
+a = 1
+b = a == (3 or 2)
+print(b)
